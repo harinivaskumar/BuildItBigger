f0ea070a6e661d3ec710ffa56baddb6cd6a1c48e
harinivaskumar <harinivaskumar@live.com>
2016-05-29 22:42:43 +0530

    Added support for Interstitial Ad

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -11,8 +11,10 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.harinivaskumarrp.android.library.JokeDisplayActivity;
 
 
@@ -25,6 +27,12 @@ public class MainActivityFragment extends Fragment
     private static final String LOG_TAG = MainActivityFragment.class.getSimpleName();
     private Button mJokeButton;
     private ProgressBar mProgressBar;
+    private AdView mAdView;
+    private InterstitialAd mInterstitialAd;
+    private String mJokeString;
+
+    private final int ADVIEW_TYPE_BANNER = 1;
+    private final int ADVIEW_TYPE_INTERSTITIAL = 2;
 
     public MainActivityFragment() {
     }
@@ -37,19 +45,45 @@ public class MainActivityFragment extends Fragment
         mJokeButton = (Button) root.findViewById(R.id.tell_joke_button);
         mJokeButton.setOnClickListener(this);
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
+        mAdView = (AdView) root.findViewById(R.id.adView);
+        loadAd(ADVIEW_TYPE_BANNER);
 
         mProgressBar = (ProgressBar) root.findViewById(R.id.progressBar);
         mProgressBar.setVisibility(View.GONE);
 
+        if (Utility.isNetworkAvailable(getContext(), LOG_TAG)){
+            mInterstitialAd = new InterstitialAd(getActivity());
+            mInterstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
+            mInterstitialAd.setAdListener(new AdListener() {
+                @Override
+                public void onAdClosed() {
+                    Log.d(LOG_TAG, "AD-Interstitial Closed! Joke Activity will be Started.");
+                    loadAd(ADVIEW_TYPE_INTERSTITIAL);
+                    startJokeActivity();
+                }
+            });
+            loadAd(ADVIEW_TYPE_INTERSTITIAL);
+        }
+
         return root;
+    }
+
+    private void loadAd(int adType) {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        switch (adType){
+            case ADVIEW_TYPE_BANNER:
+                mAdView.loadAd(adRequest);
+                Log.d(LOG_TAG, "Load AD-Banner Requested.");
+                break;
+            case ADVIEW_TYPE_INTERSTITIAL:
+                mInterstitialAd.loadAd(adRequest);
+                Log.d(LOG_TAG, "Load AD-Interstitial Requested.");
+                break;
+            default:
+                Log.e(LOG_TAG, "Unsupported AdType!");
+        }
     }
 
     @Override
@@ -59,6 +93,10 @@ public class MainActivityFragment extends Fragment
 
     public void displayJoke(){
         if (Utility.isNetworkAvailable(getContext(), LOG_TAG)) {
+            if (!mInterstitialAd.isLoaded() && mInterstitialAd.isLoading()){
+                loadAd(ADVIEW_TYPE_INTERSTITIAL);
+            }
+
             mProgressBar.setVisibility(View.VISIBLE);
             Toast.makeText(getActivity(), R.string.please_wait, Toast.LENGTH_SHORT).show();
             new EndpointsAsyncTask(getContext(), this, "HariNivas-" + R.string.flavour)
@@ -70,15 +108,29 @@ public class MainActivityFragment extends Fragment
 
     @Override
     public void getJokeResult(String joke) {
+        mJokeString = joke;
+        if (mInterstitialAd.isLoaded()) {
+            Log.d(LOG_TAG, "Load AD-Interstitial Success! Ad will be displayed.");
+            mInterstitialAd.show();
+        }
+        else {
+            loadAd(ADVIEW_TYPE_INTERSTITIAL);
+            Log.d(LOG_TAG, "Load AD-Interstitial Failure! Joke Activity will be displayed.");
+            startJokeActivity();
+        }
+    }
+
+    private void startJokeActivity(){
         mProgressBar.setVisibility(View.GONE);
-        if (null == joke){
+        if (null == mJokeString){
             Toast.makeText(getActivity(), R.string.no_joke, Toast.LENGTH_SHORT).show();
             Log.e(LOG_TAG, "Joke String is null!");
         }
         else {
             Intent jokeDisplayIntent = new Intent(getActivity(), JokeDisplayActivity.class);
             //jokeDisplayIntent.putExtra(Intent.EXTRA_TEXT, Joke.getJoke());
-            jokeDisplayIntent.putExtra(Intent.EXTRA_TEXT, joke);
+            jokeDisplayIntent.putExtra(Intent.EXTRA_TEXT, mJokeString);
+            Log.d(LOG_TAG, "Joke Activity Intent Triggered!");
             startActivity(jokeDisplayIntent);
         }
     }
