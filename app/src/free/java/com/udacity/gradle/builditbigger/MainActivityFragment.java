6cd3b92bf13f8a6ca554c78fba290201f976847b
harinivaskumar <harinivaskumar@live.com>
2016-05-29 22:07:46 +0530

    Progress bar added before displaying a Joke!

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,19 +1,30 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.harinivaskumarrp.android.library.JokeDisplayActivity;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment
+        implements View.OnClickListener, EndpointsAsyncTask.JokeListener{
+
+    private static final String LOG_TAG = MainActivityFragment.class.getSimpleName();
+    private Button mJokeButton;
+    private ProgressBar mProgressBar;
 
     public MainActivityFragment() {
     }
@@ -23,6 +34,9 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
+        mJokeButton = (Button) root.findViewById(R.id.tell_joke_button);
+        mJokeButton.setOnClickListener(this);
+
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
@@ -31,6 +45,41 @@ public class MainActivityFragment extends Fragment {
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
+
+        mProgressBar = (ProgressBar) root.findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.GONE);
+
         return root;
+    }
+
+    @Override
+    public void onClick(View view) {
+        displayJoke();
+    }
+
+    public void displayJoke(){
+        if (Utility.isNetworkAvailable(getContext(), LOG_TAG)) {
+            mProgressBar.setVisibility(View.VISIBLE);
+            Toast.makeText(getActivity(), R.string.please_wait, Toast.LENGTH_SHORT).show();
+            new EndpointsAsyncTask(getContext(), this, "HariNivas-" + R.string.flavour)
+                    .execute();
+        }else{
+            Toast.makeText(getActivity(), R.string.no_internet, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void getJokeResult(String joke) {
+        mProgressBar.setVisibility(View.GONE);
+        if (null == joke){
+            Toast.makeText(getActivity(), R.string.no_joke, Toast.LENGTH_SHORT).show();
+            Log.e(LOG_TAG, "Joke String is null!");
+        }
+        else {
+            Intent jokeDisplayIntent = new Intent(getActivity(), JokeDisplayActivity.class);
+            //jokeDisplayIntent.putExtra(Intent.EXTRA_TEXT, Joke.getJoke());
+            jokeDisplayIntent.putExtra(Intent.EXTRA_TEXT, joke);
+            startActivity(jokeDisplayIntent);
+        }
     }
 }
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,18 +1,12 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
-import com.harinivaskumarrp.android.library.JokeDisplayActivity;
 
 
-public class MainActivity extends AppCompatActivity implements EndpointsAsyncTask.JokeListener{
+public class MainActivity extends AppCompatActivity{
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
@@ -43,28 +37,5 @@ public class MainActivity extends AppCompatActivity implements EndpointsAsyncTas
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void displayJoke(View view){
-        if (Utility.isNetworkAvailable(this, LOG_TAG)) {
-            new EndpointsAsyncTask(this, this, "HariNivas")
-                    .execute();
-        }else{
-            Toast.makeText(this, "No Internet Connection!", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    @Override
-    public void getJokeResult(String joke) {
-        if (null == joke){
-            Toast.makeText(this, "No Joke currently! try after some time.", Toast.LENGTH_SHORT).show();
-            Log.e(LOG_TAG, "Joke String is null!");
-        }
-        else {
-            Intent jokeDisplayIntent = new Intent(this, JokeDisplayActivity.class);
-            //jokeDisplayIntent.putExtra(Intent.EXTRA_TEXT, Joke.getJoke());
-            jokeDisplayIntent.putExtra(Intent.EXTRA_TEXT, joke);
-            startActivity(jokeDisplayIntent);
-        }
     }
 }
