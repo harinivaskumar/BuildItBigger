591c4f1d31d8949dd3662f83c082670db75d6b57
harinivaskumar <harinivaskumar@live.com>
2016-05-29 19:35:07 +0530

    Joke Back End Test Case 2 Added

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeGCMBackEndTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/JokeGCMBackEndTest.java
@@ -3,19 +3,37 @@ package com.udacity.gradle.builditbigger;
 import android.test.AndroidTestCase;
 import android.util.Log;
 
+import java.util.concurrent.ExecutionException;
+
 /**
  * Created by Hari Nivas Kumar R P on 5/28/2016.
  */
 public class JokeGCMBackEndTest extends AndroidTestCase implements EndpointsAsyncTask.JokeListener{
     private static final String LOG_TAG = JokeGCMBackEndTest.class.getSimpleName();
+    private String jokeStr;
 
-    public void testBackend() {
-        new EndpointsAsyncTask(mContext, this,"HariNivas").execute();
+    public void testBackend() throws ExecutionException, InterruptedException{
+        new EndpointsAsyncTask(mContext, this,"Hari").execute();
     }
 
     @Override
     public void getJokeResult(String jokeString) {
         Log.d(LOG_TAG, "Joke is - " + jokeString + " and Length - " + jokeString.length());
         assertTrue(jokeString, jokeString.length() > 0);
+    }
+
+    public void testBackend2() throws ExecutionException, InterruptedException {
+        new EndpointsAsyncTask(mContext,
+                new EndpointsAsyncTask.JokeListener() {
+                    @Override
+                    public void getJokeResult(String jokeResult) {
+                        jokeStr = jokeResult;
+                    }
+                },
+                "Nivas")
+                .execute();
+        
+        Log.d(LOG_TAG, "Joke is - " + jokeStr + " and Length - " + jokeStr.length());
+        assertTrue(jokeStr, jokeStr.length() > 0);
     }
 }
